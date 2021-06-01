da8c9b423d8a84e96fea2a5c4cf84b92b74f9b68
Erica-ui <UP937922@myport.ac.uk>
2021-06-01 18:31:02 +0100

    test

--- Game.js
+++ Game.js
@@ -22,7 +22,7 @@ Main_length =  Users_Guess;
 for (i = 0; i <= Main_length; i++) {
   if (User_Guess == Main_length) {
   Users_Guess += 1 [i]
-  res.send("well done")
+  res.send("well done");
 } else {
   res.send("Incorrect");
 }}
