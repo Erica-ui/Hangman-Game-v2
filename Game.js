1d89f585d86d61bd4c72f88a14bd844fdbe0c13c
Erica-ui <UP937922@myport.ac.uk>
2021-06-01 14:13:10 +0100

    First Commit

---
+++ Game.js
@@ -0,0 +1,19 @@
+// const http = require('http')
+const express = require('express')
+const port = 3000
+
+// const server = http.createServer();
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+app.listen(port, () => console.log('Server is listening on port', + port) );
+// server.listen(port, function (error) {
+//   if (error) {
+//     console.log('Something went wrong', error)
+//   } else {
+//     console.log('Server is listening on port', + port)
+//    } 
+// });
